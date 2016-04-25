52873be8321dc0c7aeca96815e2797922e18e82f
Sharmi <rai.sharmistha@gmail.com>
2016-04-25 20:29:10 +0200

    change type to SSML but test fails

--- src/index.js
+++ src/index.js
@@ -132,10 +132,10 @@ function handleNewFactRequest(response) {
 
     // Create speech output
     var speechOutput = {
-      speech: "<speak> For your reflection: <break time="2s"/> "
+      speech: "<speak> For your reflection:"
         + "<audio src='https://s3.amazonaws.com/findyourzen/Bell.mp3'/> "
         + fact
-        + "<break time="2s"/>"
+//        + "<break time="2s"/>"
         + "<audio src='https://s3.amazonaws.com/findyourzen/Bell.mp3'/> "
         + "</speak>",
       type: AlexaSkill.speechOutputType.SSML
